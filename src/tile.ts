a03b5113c939fa7fca17cf99cd633deb9097a40b
kjk <kody.j.king@gmail.com>
2021-06-12 15:38:58 -0700

    temporarily removing source to fix problems caused by fucking case insensitivity

--- src/tile.ts
+++
@@ -1,9 +0,0 @@
-export default class Tile {
-    //Really could be anything!
-    //set to number for temporary convienance
-    content: number
-    //temporary 0 to model empty
-    constructor(content = 0) {
-        this.content = content
-    }
-}
